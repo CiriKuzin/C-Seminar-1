009cfcee850563ac2716ae6c537167bab174c601
Cirill <c.cuzi@gmail.ru>
2023-06-12 21:26:10 +0300

    dz2

---
+++ task 6 dz2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB && numberA > numberC)
+{
+    Console.Write($"Наибольшее число {numberA}");
+}
+else if (numberB > numberA && numberB > numberC)
+{
+    Console.Write($"Наибольшее число {numberB}");
+}
+else
+{
+    Console.Write($"Наибольшее число {numberC}");
+}
