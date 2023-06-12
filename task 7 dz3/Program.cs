852edd3f4a53910b01c4a129ef69e4bd720559de
Cirill <c.cuzi@gmail.ru>
2023-06-12 21:34:17 +0300

    dz3

---
+++ task 7 dz3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = numberA % 2;
+
+if (numberB == 0)
+{
+    Console.Write($"Число {numberA} чётное");
+}
+else 
+{
+    Console.Write($"Число {numberA} нечётное");
+}
