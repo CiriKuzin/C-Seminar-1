675deb6f5a95118c592990ff5ac6564a5b157967
Cirill <c.cuzi@gmail.ru>
2023-06-12 21:45:03 +0300

    dz4

---
+++ task 8 dz4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int x = 0;
+
+if (number % 2 == 0)
+    {
+        while (x <= 2 * number)
+        {
+            Console.Write($"{-number+x}, ");
+            x = x + 2;
+        }
+    }
+    else
+    {
+        x = x + 1;
+        while (x <= 2 * number)
+        {
+            Console.Write($"{-number+x}, ");
+            x = x + 2;
+        }
+    }
