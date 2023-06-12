3c1c224a92031801355ad4912bd2ae7a70e14f72
Cirill <c.cuzi@gmail.ru>
2023-06-12 20:07:30 +0300

    task 1

---
+++ task 1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+
+if (NumberA == NumberB * NumberB)
+{
+    Console.Write($"Число {NumberA} является квадратом числа {NumberB}");
+}
+else
+{
+    Console.Write($"Число {NumberA} не является квадратом числа {NumberB}");
+}
