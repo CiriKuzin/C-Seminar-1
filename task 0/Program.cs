03b073bd5515ec9e0a093e64c0edd97a2ab01feb
Cirill <c.cuzi@gmail.ru>
2023-06-12 19:57:36 +0300

    task 0

---
+++ task 0/Program.cs
@@ -0,0 +1,7 @@
+﻿// Расчёт квадрата числа
+// Convert.ToInt32(Console.ReadLine()) - функция для чтения чисел (перевод строки в число)
+// $ - ссылка на переменные
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int quater = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {quater}");
