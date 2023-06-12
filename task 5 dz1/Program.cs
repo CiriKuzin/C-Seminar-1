46a59d702c0e104bf50eceead4ab930bdab7ffa3
Cirill <c.cuzi@gmail.ru>
2023-06-12 21:19:03 +0300

    dz1

---
+++ task 5 dz1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+
+if (NumberA > NumberB)
+{
+    Console.WriteLine($"Число {NumberA} больше числа {NumberB}");
+}
+else
+{
+    Console.WriteLine($"Число {NumberB} больше числа {NumberA}");
+}
