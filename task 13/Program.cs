6fa558bfb42a5c77432e77bbdd08f9b327eb7610
Cirill <c.cuzi@gmail.ru>
2023-06-17 16:35:35 +0300

    task 13

---
+++ task 13/Program.cs
@@ -0,0 +1,18 @@
+﻿void SqrSearch(int num1, int num2)
+{
+    if (num1 == num2 * num2 || num2 == num1 * num1)
+    {
+        System.Console.WriteLine("ДА");
+    }
+    else
+    {
+        System.Console.WriteLine("НЕТ");;
+    }
+}
+
+
+System.Console.Write("Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+SqrSearch(number1, number2);
