3f6be16478ed32e047643751c68313f643382926
Cirill <c.cuzi@gmail.ru>
2023-06-17 16:46:36 +0300

    task 15

--- task 10/Program.cs
+++ task 10/Program.cs
@@ -8,6 +8,6 @@
     return result;
 }
 
-int number = new Random().Next(100,999);
+int number = new Random().Next(100, 1000);
 System.Console.WriteLine($"Ваше случайное число: {number}");
 System.Console.WriteLine($"Получившийся результат от удаление второй цифры из числа {number} равен {DelMedian(number)}");
---
+++ task 14 dz5/Program.cs
@@ -0,0 +1,9 @@
+﻿int FindMedian(int num)
+{
+    int result = (num % 100) / 10;
+    return result;
+}
+
+int number = new Random().Next(100,1000);
+System.Console.WriteLine($"Ваше случайное число: {number}");
+System.Console.WriteLine($"Средняя цифра числа {number} равна {FindMedian(number)}");
