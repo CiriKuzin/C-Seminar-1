a3531ddefc149f9be703daace819156c6a36d1eb
Cirill <c.cuzi@gmail.ru>
2023-06-17 16:05:33 +0300

    task 10

---
+++ task 10/Program.cs
@@ -0,0 +1,13 @@
+﻿int DelMedian(int num)
+{
+    int num1 = num / 100;
+    int num2 = num / 10;
+    int num3 = num % num2;
+    int result = 0;
+    result = num1*10 + num3;
+    return result;
+}
+
+int number = new Random().Next(100,999);
+System.Console.WriteLine($"Ваше случайное число: {number}");
+System.Console.WriteLine($"Получившийся результат от удаление второй цифры из числа {number} равен {DelMedian(number)}");
