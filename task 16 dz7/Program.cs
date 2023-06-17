8802801b0a890b8a3a7a277fa56f3e99d797c79f
Cirill <c.cuzi@gmail.ru>
2023-06-17 18:53:38 +0300

    task 17

---
+++ task 16 dz7/Program.cs
@@ -0,0 +1,18 @@
+﻿void Week(int num)
+{
+    if (num < 1 || num >7)
+    {
+        System.Console.Write("Дня недели с таким номером нет");
+    }
+    else if (num == 6 || num == 7)
+    {
+        System.Console.Write("ДА");
+    }
+    else
+    {
+        System.Console.Write("НЕТ");
+    }
+}
+System.Console.Write("Число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Week(number);
