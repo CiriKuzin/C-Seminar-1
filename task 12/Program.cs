c35b5cd610439f41a2d24c40b933ae40fd0bc7b5
Cirill <c.cuzi@gmail.ru>
2023-06-17 16:28:53 +0300

    task 12

---
+++ task 12/Program.cs
@@ -0,0 +1,16 @@
+﻿void CheckForMult(int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        System.Console.WriteLine("ДА");
+    }
+    else
+    {
+        System.Console.WriteLine("НЕТ");;
+    }
+}
+
+
+System.Console.Write("Число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CheckForMult(number);
