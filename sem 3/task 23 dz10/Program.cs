eb27f7754c315b3d8ccd09bbc2e63dd4df77ae08
Cirill <c.cuzi@gmail.ru>
2023-06-18 11:29:43 +0300

    task 23

---
+++ sem 3/task 23 dz10/Program.cs
@@ -0,0 +1,10 @@
+﻿void TableOfCube(int num)
+{
+    for (int i = 0; i < num; i++)
+    System.Console.WriteLine($"{i+1} * {i+1} = {Math.Pow(i+1, 3)}");
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableOfCube(number);
