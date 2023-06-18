889ecc8bf2ab3ace9b02e84e19b6ac26762b87b1
Cirill <c.cuzi@gmail.ru>
2023-06-18 11:18:14 +0300

    task 21

---
+++ sem 3/task 21 dz8/Program.cs
@@ -0,0 +1,23 @@
+﻿void Palindrome(int num)
+{
+    int num1 = num / 10000;
+    int num2 = num % 10000 / 1000;
+    int num3 = num % 1000 / 100;
+    int num4 = num % 100 / 10;
+    int num5 = num % 10;
+    if (num1 == num5 && num2 == num4)
+    {
+        System.Console.WriteLine("Ваше число палиндром");
+    }
+    else
+    {
+        System.Console.WriteLine("Ваше число не палиндром");
+    }
+}
+
+// int number = new Random().Next(10000, 100000);
+// System.Console.WriteLine($"Ваше случайное число: {number}");
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Palindrome(number);
