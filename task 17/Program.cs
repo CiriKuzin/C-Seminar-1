c8c332abf5cc21dca9a912be482d305ebf120b87
Cirill <c.cuzi@gmail.ru>
2023-06-18 10:18:36 +0300

    task 17

---
+++ task 17/Program.cs
@@ -0,0 +1,26 @@
+﻿void CoorArea(int num1, int num2)
+{
+    if (num1 > 0 && num2 > 0)
+    {
+        System.Console.Write("Точка располагается в первой четверти");
+    }
+    else if (num1 < 0 && num2 > 0)
+    {
+        System.Console.Write("Точка располагается во второй четверти");
+    }
+    else if (num1 < 0 && num2 < 0)
+    {
+        System.Console.Write("Точка располагается в третьей четверти");
+    }
+    else
+    {
+        System.Console.Write("Точка располагается в четвертой четверти");
+    }
+}
+
+System.Console.Write("Введите значение X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите значение Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+CoorArea(x,y);
