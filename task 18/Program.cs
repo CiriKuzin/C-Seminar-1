867e4a8a3f3b6a58683f624c89c01d95f0a2d76e
Cirill <c.cuzi@gmail.ru>
2023-06-18 10:29:29 +0300

    task 18

---
+++ task 18/Program.cs
@@ -0,0 +1,29 @@
+﻿void PossCoor(int num)
+{
+    if (num == 1)
+    {
+        System.Console.Write("Диапазон точек: {x>0;y>0}");
+    }
+    else if (num == 2)
+    {
+        System.Console.Write("Диапазон точек: {x<0;y>0}");
+    }
+    else if (num == 3)
+    {
+        System.Console.Write("Диапазон точек: {x<0;y<0}");
+    }
+    else if (num == 4)
+    {
+        System.Console.Write("Диапазон точек: {x>0;y<0}");
+    }
+    else
+    {
+        System.Console.Write("Области с таким номером нет в двумерной системе координат");
+    }
+}
+
+System.Console.Write("Введите номер координатной четверти: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+PossCoor(number);
