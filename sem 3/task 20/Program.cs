02ab3849d323bde5c48aed7677091dcbd73f83b6
Cirill <c.cuzi@gmail.ru>
2023-06-18 11:04:58 +0300

    task 20

---
+++ sem 3/task 20/Program.cs
@@ -0,0 +1,10 @@
+﻿void TableOfSqr(int num)
+{
+    for (int i = 0; i < num; i++)
+    System.Console.WriteLine($"{i+1} * {i+1} = {Math.Pow(i+1, 2)}");
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+TableOfSqr(number);
