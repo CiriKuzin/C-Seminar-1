3358a1f6258a49c92a48f622cf363221f8c27582
Cirill <c.cuzi@gmail.ru>
2023-06-24 19:34:33 +0300

    task 32

---
+++ sem 5/task 32/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    Random a = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = a.Next(-9 , 10);
+    }
+    return arr;
+}
+
+System.Console.Write("Задайте размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
+System.Console.WriteLine($"Начальный массив: [{string.Join("; ", array)}]");
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = - array[i];
+}
+System.Console.WriteLine($"Реверсивный массив: [{string.Join("; ", array)}]");
