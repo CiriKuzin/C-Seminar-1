c3d1b5563d4ec8a9e759445d8b87d552280cf952
Cirill <c.cuzi@gmail.ru>
2023-06-24 15:46:50 +0300

    task 30dz13

---
+++ sem 4/task 30 dz13/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("Число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+Random a = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = a.Next(-100 , 100);
+}
+System.Console.WriteLine($"[{string.Join("; ", array)}]");
