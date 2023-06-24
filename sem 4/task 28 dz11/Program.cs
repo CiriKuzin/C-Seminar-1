dc987ee94c1954d3c7372a7f725ecbe751a99228
Cirill <c.cuzi@gmail.ru>
2023-06-24 14:45:31 +0300

    task 28dz11

---
+++ sem 4/task 28 dz11/Program.cs
@@ -0,0 +1,17 @@
+﻿int APowB(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 0; i < num2; i++)
+    {
+        result = result * num1;
+    }
+System.Console.WriteLine($"{num1} ^ {num2} = {result}");
+return result;
+}
+
+System.Console.Write("Введите 1 число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите 2 число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+APowB(numberA, numberB);
