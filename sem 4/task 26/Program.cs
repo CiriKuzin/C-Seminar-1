dc176fff72f81f68f11d1be15de1e7979fe43388
Cirill <c.cuzi@gmail.ru>
2023-06-24 13:59:07 +0300

    task 26

--- sem 4/task 26/Program.cs
+++ sem 4/task 26/Program.cs
@@ -1,15 +1,15 @@
-﻿int MultToN(int n)
+﻿int MultToN(int num)
 {
     int result = 1;
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= num; i++)
     {
         result = result * i;
     }
-    System.Console.WriteLine($"Произведение чисел от 1 до {n} равно {result}");
+    System.Console.WriteLine($"Произведение чисел от 1 до {num} равно {result}");
     return result;
 }
 
 System.Console.Write("Введите число: ");
-int N = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
-MultToN(N);
+MultToN(number);
