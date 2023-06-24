c117e6bf95460868443549d2b257fd52ff4ccce3
Cirill <c.cuzi@gmail.ru>
2023-06-24 21:37:11 +0300

    task 36dz14

---
+++ sem 5/task 36 dz14/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    Random a = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = a.Next(100 , 1000);
+    }
+    return arr;
+}
+
+System.Console.Write("Задайте размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
+System.Console.WriteLine($"[{string.Join("; ", array)}]");
+
+int result = 0;
+ for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        result = result + 1;
+    }
+}
+System.Console.WriteLine($"Количество четных чисел в масиве : {result}");
