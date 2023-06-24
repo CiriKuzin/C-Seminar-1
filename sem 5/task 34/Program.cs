5166b6c58b9d990f14ab59dbdd7dd7cdf3a943b7
Cirill <c.cuzi@gmail.ru>
2023-06-24 20:43:52 +0300

    task 34

---
+++ sem 5/task 34/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    Random a = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = a.Next(0 , 1000);
+    }
+    return arr;
+}
+
+System.Console.Write("Задайте размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+int[] array = CreateArray(length);
+System.Console.WriteLine($"[{string.Join("; ", array)}]");
+System.Console.WriteLine();
+System.Console.Write("Введите нижний предел диапазона: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите нижний предел диапазона: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] >= min &&  array[i] <= max)
+    {
+        result = result + 1;
+    }
+}
+System.Console.WriteLine();
+System.Console.Write($"Массив содержит {result} элементов находящихся в диапазоне [{min},{max}]");
