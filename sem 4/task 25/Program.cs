88bf261fc405093d676dd7c49e4274ce13aec850
Cirill <c.cuzi@gmail.ru>
2023-06-24 13:44:56 +0300

    task 25

---
+++ sem 4/task 25/Program.cs
@@ -0,0 +1,10 @@
+﻿void Count(int num)
+{
+    int length = num.ToString().Length;
+    System.Console.WriteLine($"Количество цифр в числе {num} равно {length}");
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Count(number);
