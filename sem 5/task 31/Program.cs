d07518b56a6541a5e9545ab0743eecbc4dd102cf
Cirill <c.cuzi@gmail.ru>
2023-06-24 19:13:03 +0300

    task 31

---
+++ sem 5/task 31/Program.cs
@@ -0,0 +1,21 @@
+﻿System.Console.Write("Задайте размер массива: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+Random a = new Random();
+int pos = 0;
+int neg = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = a.Next(-9 , 10);
+    if (array[i] > 0)
+    {
+        pos = pos + array[i];
+    }
+    else
+    {
+        neg = neg + array[i];
+    }
+}
+System.Console.WriteLine($"[{string.Join("; ", array)}]");
+System.Console.WriteLine($"Cумма положительных чисел: {pos}");
+System.Console.WriteLine($"Cумма отрицательных чисел: {neg}");
