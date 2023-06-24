023e12e3acc698df172fe240b0dd6fa3145d5d7b
Cirill <c.cuzi@gmail.ru>
2023-06-24 15:39:25 +0300

    task 29dz12

---
+++ sem 4/task 29 dz12/Program.cs
@@ -0,0 +1,24 @@
+﻿int SumNum(int num)
+{
+    int length = num.ToString().Length;
+    int[] array = new int [length];
+    int result = array[0];
+    int a = 1;
+    for (int i = 0; i < length; i++)
+    {
+        a = a * 10;
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = (num % a) / (a / 10);
+        a = a / 10;
+        result = result + array[i];
+    }
+    System.Console.WriteLine($"Cумма цифр числа {num} равна {result}");
+    return result;
+}
+
+System.Console.Write("Число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+SumNum(number);
