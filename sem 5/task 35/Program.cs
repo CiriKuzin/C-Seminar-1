910f148346adbaeef43702ef85e7f5dce7d584ae
Cirill <c.cuzi@gmail.ru>
2023-06-24 21:28:29 +0300

    task 35

---
+++ sem 5/task 35/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    Random a = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = a.Next(0 , 10);
+    }
+    return arr;
+}
+
+System.Console.Write("Задайте размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
+System.Console.WriteLine($"[{string.Join("; ", array)}]");
+
+int Newlenght = array.Length / 2 + array.Length % 2;
+int[] NewArray = new int[Newlenght];
+
+for (int i = 0; i < array.Length / 2; i++)
+{
+    NewArray[i] = array[i] * array[array.Length - i - 1];
+}
+if (NewArray.Length % 2 == 1)
+{
+    NewArray[Newlenght - 1] = array[Newlenght - 1];
+}
+System.Console.WriteLine($"[{string.Join("; ", NewArray)}]");
