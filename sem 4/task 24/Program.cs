5a9027234951dde3b91efaf5d7f19ea6e1bc5c47
Cirill <c.cuzi@gmail.ru>
2023-06-24 13:35:48 +0300

    task 24

---
+++ sem 4/task 24/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumToA(int a)
+{
+    int result = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        result = result + i;
+    }
+    System.Console.WriteLine($"Сумма чисел от 1 до {a} равна {result}");
+    return result;
+}
+
+System.Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+SumToA(A);
