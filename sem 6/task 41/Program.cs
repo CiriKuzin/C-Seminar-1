83783ceffb0428e19966bf43024dcbbd1a003b53
Cirill <c.cuzi@gmail.ru>
2023-07-02 15:27:27 +0300

    task 41

---
+++ sem 6/task 41/Program.cs
@@ -0,0 +1,16 @@
+﻿void GetBinaryViev (int num)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    else
+    {
+        GetBinaryViev(num / 2);
+        System.Console.Write(num % 2);
+    }
+}
+
+System.Console.Write("Введите число в десятичной системе: ");
+int length = Convert.ToInt32(Console.ReadLine());
+GetBinaryViev(length);
