f03d5762cbd36e4a2e3f813d54495221fafc0fec
Cirill <c.cuzi@gmail.ru>
2023-07-02 20:19:02 +0300

    task 51dz19

---
+++ sem 7/task 51dz19/Program.cs
@@ -0,0 +1,31 @@
+﻿void CreateRandomMatrix (double [,] matrix)
+{
+    Random rnd = new Random ();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(rnd.Next(-99,100) + rnd.NextDouble(),2);
+        }
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write( $"{matrix[i,j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+double [,] matrix = new double [row, column];
+CreateRandomMatrix(matrix);
+PrintMatrix(matrix);
