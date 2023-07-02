4f002055d8debc84ccbb957be633a8097709e42b
Cirill <c.cuzi@gmail.ru>
2023-07-02 14:55:18 +0300

    task 40

---
+++ sem 6/task 40/Program.cs
@@ -0,0 +1,19 @@
+﻿void Triangle(int a, int b, int c)
+{
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        System.Console.WriteLine($"Треугольник со сторонами {a}, {b} и {c} существует");  
+    }
+    else
+    {
+        System.Console.WriteLine($"Треугольника со сторонами {a}, {b} и {c} не существует");
+    }
+}
+
+int number1 = new Random().Next(0,100);
+System.Console.WriteLine($"Первая сторона треугольника: {number1}");
+int number2 = new Random().Next(0,100);
+System.Console.WriteLine($"Первая сторона треугольника: {number2}");
+int number3 = new Random().Next(0,100);
+System.Console.WriteLine($"Первая сторона треугольника: {number3}");
+Triangle(number1, number2, number3);
