697c609610b0e60ab6c5fe6e27136d234f05e8c3
Cirill <c.cuzi@gmail.ru>
2023-07-02 18:40:29 +0300

    task 48

---
+++ sem 7/task 48/Program.cs
@@ -0,0 +1,38 @@
+﻿void CreateRandomMatrix (int [,] matrix)
+{
+    Random rnd = new Random ();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0,10);
+            System.Console.Write($"{matrix[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void SqrMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i,j] = matrix[i,j] * matrix[i,j];
+            }
+            System.Console.Write($"{matrix[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int [row, column];
+CreateRandomMatrix(matrix);
+System.Console.WriteLine();
+SqrMatrix(matrix);
