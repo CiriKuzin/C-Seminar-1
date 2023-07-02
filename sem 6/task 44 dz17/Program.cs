d7bae6908049915389c0c7c3261cd75984943d49
Cirill <c.cuzi@gmail.ru>
2023-07-02 16:24:49 +0300

    task 44dz17

---
+++ sem 6/task 44 dz17/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] CreateArray(int size)
+{
+    int[] arr = new int[size];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        if (arr[i] > 0)
+        {
+            count = count + 1;
+        }
+    }
+    System.Console.Write($"Среди чисел {count} положительных");
+    return arr;
+}
+
+System.Console.Write("Задайте размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(length);
