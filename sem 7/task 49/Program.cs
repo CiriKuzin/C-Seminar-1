15c59e57aa6c8b5b27657aa44dd39762f04994e8
Cirill <c.cuzi@gmail.ru>
2023-07-02 19:21:56 +0300

    task 49

--- sem 7/task 49/Program.cs
+++ sem 7/task 49/Program.cs
@@ -14,18 +14,21 @@
 
 int DiagMatrix(int[,] matrix)
 {
-    int count = 0;
+    int sum = 0;
+    System.Console.Write("Числа находящиеся на главной диагонали: ");
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             if (i == j )
             {
-                count = count + matrix[i,j];
+                sum = sum + matrix[i,j];
+                System.Console.Write(matrix[i,j] + " ");
             }
         }
     }
-    return count;
+     System.Console.WriteLine();
+    return sum;
 }
 
 System.Console.Write("Введите кол-во строк: ");
@@ -35,4 +38,4 @@ int column = Convert.ToInt32(Console.ReadLine());
 int[,] matrix = new int [row, column];
 CreateRandomMatrix(matrix);
 System.Console.WriteLine();
-System.Console.Write($"Сумма элементов находящихся на главной диагонали: {DiagMatrix(matrix)}");
+System.Console.WriteLine($"Сумма элементов находящихся на главной диагонали: {DiagMatrix(matrix)}");
