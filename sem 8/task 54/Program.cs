505f74c173ae70f27ae5c138605ccaca5ee843d5
Cirill <c.cuzi@gmail.ru>
2023-07-06 18:52:39 +0300

    task 54

---
+++ sem 8/task 54/Program.cs
@@ -0,0 +1,34 @@
+﻿void CreateMatrix (int [,] matrix)
+{
+    Random rnd = new Random ();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(0,10);
+            System.Console.Write($"{matrix[i,j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+System.Console.Write("Введите кол-во строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите кол-во столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int [row, column];
+CreateMatrix(matrix);
+
+System.Console.WriteLine();
+
+int[,] resultMatrix = new int [column, row];
+
+for (int i = 0; i < resultMatrix.GetLength(0); i++)
+{
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+        resultMatrix[i, j] = matrix[j,i];
+        System.Console.Write($"{resultMatrix[i,j]} ");
+    }
+    System.Console.WriteLine();
+}
