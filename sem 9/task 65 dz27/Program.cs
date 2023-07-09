2e794dde50e108898047cf68392e909aeeba491e
Cirill <c.cuzi@gmail.ru>
2023-07-09 18:04:52 +0300

    task 65dz27

---
+++ sem 9/task 65 dz27/Program.cs
@@ -0,0 +1,13 @@
+﻿void NumberToOne (int num)
+{ 
+    if (num == 0)
+    {
+        return;
+    }
+    System.Console.Write($"{num} ");
+    NumberToOne(num - 1);
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NumberToOne(number);
