c42372c728f9dfecbc85befeae28f9654a0f2344
Cirill <c.cuzi@gmail.ru>
2023-07-09 19:12:23 +0300

    task 67dz29

---
+++ sem 9/task 67 dz29/Program.cs
@@ -0,0 +1,12 @@
+﻿int AckermanFunction (int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return AckermanFunction(m-1, 1);
+    return AckermanFunction(m-1, AckermanFunction(m, n - 1));
+}
+
+System.Console.Write("Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write(AckermanFunction(number1, number2));
