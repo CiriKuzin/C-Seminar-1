48a6fdc31a8cf4c11923910dfd75206e5fb8f6fd
Cirill <c.cuzi@gmail.ru>
2023-07-09 17:39:06 +0300

    task 63

---
+++ sem 9/task 63/Program.cs
@@ -0,0 +1,12 @@
+﻿int SumToNumber (int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    return num % 10 + SumToNumber(num / 10);
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.Write($"Сумма цифр числа {number} равна {SumToNumber(number)}");
