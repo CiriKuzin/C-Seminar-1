1727e21d816992b18e28678a0d0ae601ded5ea7d
Cirill <c.cuzi@gmail.ru>
2023-07-09 18:13:48 +0300

    task 66dz28

---
+++ sem 9/task 66 dz28/Program.cs
@@ -0,0 +1,14 @@
+﻿int SumOfNumberMN (int num1, int num2)
+{
+    if (num2 - num1 == -1)
+    {
+        return 0;
+    }
+    return num2 += SumOfNumberMN(num1, num2 - 1);
+}
+
+System.Console.Write("Нижняя граница: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Верхняя граница: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write($"Сумма чисел от {number1} до {number2} равна {SumOfNumberMN(number1, number2)}");
