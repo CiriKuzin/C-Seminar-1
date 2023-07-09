81cecbe408cb7f824f532d3a4f906be01c2616f3
Cirill <c.cuzi@gmail.ru>
2023-07-09 17:15:52 +0300

    task 61

---
+++ sem 9/task 61/Program.cs
@@ -0,0 +1,13 @@
+﻿void OneToNumber (int num)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    OneToNumber(num - 1);
+    System.Console.Write($"{num} ");
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+OneToNumber(number);
