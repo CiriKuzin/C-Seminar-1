401cc1b206b35079f7f2c4c254df3a5fb7adcfba
Cirill <c.cuzi@gmail.ru>
2023-07-09 17:26:09 +0300

    task 62

---
+++ sem 9/task 62/Program.cs
@@ -0,0 +1,15 @@
+﻿void NumberToNumber (int num1, int num2)
+{
+    if (num2 - num1 == -1)
+    {
+        return;
+    }
+    NumberToNumber(num1, num2 - 1);
+    System.Console.Write($"{num2} ");
+}
+
+System.Console.Write("Нижняя граница: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Верхняя граница: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+NumberToNumber (number1, number2);
